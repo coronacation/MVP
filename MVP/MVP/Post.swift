9c97bdd30df420618a047921e1d428114ac19124
David <DavidJacobsen@Davids-MacBook-Air.local>
2020-04-28 18:26:59 -0400

    added post description write to firebase

---
+++ MVP/MVP/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Post.swift
+//  MVP
+//
+//  Created by David on 4/27/20.
+//  Copyright © 2020 coronacation. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    var title: String
+    var description: String
+  //  var imageURL: URL?
+ //   let user: User
+    var viewCount: Int = 0
+    var saveCount: Int = 0
+    var flagCount: Int = 0
+    let dateCreated = Date()
+    
+    init(title: String, description: String, user: User) {
+        self.title = title
+        self.description = description
+     //   self.user = User
+    }
+    
+    // maybe add in the firebase UID for the initializer
+    
+}
