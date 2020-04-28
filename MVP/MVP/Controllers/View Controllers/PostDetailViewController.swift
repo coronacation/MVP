fe5b6512a89db2a3afe1fc1398316c7eb44a63ec
Anthroman <30359515+Anthroman@users.noreply.github.com>
2020-04-28 15:34:24 -0600

    Set up Home, Profile, and Add Post storyboards.

---
+++ MVP/MVP/Controllers/View Controllers/PostDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostDetailViewController.swift
+//  MVP
+//
+//  Created by Anthroman on 4/28/20.
+//  Copyright © 2020 coronacation. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
