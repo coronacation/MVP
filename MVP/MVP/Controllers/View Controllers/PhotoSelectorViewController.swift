8854b62cb9d0cb52530e249f99d09ca97e790f7f
Anthroman <30359515+Anthroman@users.noreply.github.com>
2020-04-29 12:02:31 -0600

    Add PhotoSelector and MARKs

---
+++ MVP/MVP/Controllers/View Controllers/PhotoSelectorViewController.swift
@@ -0,0 +1,98 @@
+//
+//  PhotoSelectorViewController.swift
+//  TalkShow
+//
+//  Created by Anthroman on 4/14/20.
+//  Copyright © 2020 JoshuaBaatz. All rights reserved.
+//
+
+import UIKit.UIImage
+
+protocol PhotoSelectorViewControllerDelegate: class {
+    func photoSelectorViewControllerSelected(image: UIImage)
+}
+
+class PhotoSelectorViewController: UIViewController {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var selectPhotoButton: UIButton!
+    
+    //MARK: - Properties
+    let imagePicker = UIImagePickerController()
+    weak var delegate: PhotoSelectorViewControllerDelegate?
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    //MARK: - Actions
+    @IBAction func selectPhotoButtonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add a photo", message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+            self.imagePicker.dismiss(animated: true, completion: nil)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            self.openCamera()
+        }
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            self.openGallery()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(cameraAction)
+        alert.addAction(photoLibraryAction)
+        
+        present(alert, animated: true, completion: nil)
+    }//end of selectPhotoButtonTapped func
+    
+    func setupViews() {
+        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.clipsToBounds = true
+        photoImageView.backgroundColor = .black
+        imagePicker.delegate = self
+    }
+}//end of PhotoSelectorViewController class
+
+extension PhotoSelectorViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "No Camera Access", message: "Please allow access to the camera to use this feature.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Back", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }//end of openCamera func
+    
+    func openGallery() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "No Photo Access", message: "Please allow access to photos to use this feature.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Back", style: .default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }//end of openGallery func
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[.originalImage] as? UIImage {
+            guard let delegate = delegate
+                else { return }
+            delegate.photoSelectorViewControllerSelected(image: pickedImage)
+            photoImageView.image = pickedImage
+            selectPhotoButton.setTitle("", for: .normal)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }//end of imagePickerController func
+}//end of PhotoSelectorViewController extension
