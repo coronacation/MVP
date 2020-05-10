20aa083ea02e387ca42dd0c17c67d33238fa11c0
Anthroman <30359515+Anthroman@users.noreply.github.com>
2020-05-10 16:54:48 -0600

    Add CurrentUserControllerExtension, Blue circles, and location randomizer

--- MVP/MVP/Controllers/View Controllers/MapViewController.swift
+++ MVP/MVP/Controllers/View Controllers/MapViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 coronacation. All rights reserved.
 //
 
+import UIKit
 import MapKit
 import CoreLocation
 
@@ -35,25 +36,18 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         checkLocationServices()
     }
     
-    func setupLocationManager() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    }
-    
-    func centerViewOnUserLocation() {
-        if let location = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: tenMiles, longitudinalMeters: tenMiles)
-            mapView.setRegion(region, animated: true)
-        }
-    }
-    
     func checkLocationServices() {
         if CLLocationManager.locationServicesEnabled() {
             setupLocationManager()
             checkLocationAuthorization()
         } else {
-            // show alert letting user know they must turn this on
+            locationServicesDisabledAlert()
         }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     func checkLocationAuthorization() {
@@ -71,21 +65,58 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             presentRestrictedAlert()
             break
         case .authorizedAlways:
-            break
+            centerViewOnUserLocation()
         @unknown default:
             print("Default case for CLLocationManager.authorizationStatus()")
         }
     }//end of checkLocationAuthorization func
-}
+
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: tenMiles, longitudinalMeters: tenMiles)
+            mapView.setRegion(region, animated: true)
+        }
+    }//end of centerViewOnUserLocation func
+}//end of MapViewController
+
+extension MapViewController {
+    func addRandomLocation(_ sender: UIBarButtonItem) {
+        let radius = 1200
+        let randomCenter = CurrentUserController.shared.generateRandomLocationForUser().coordinate
+        let circle = MKCircle(center: randomCenter, radius: CLLocationDistance(radius))
+        mapView.addOverlay(circle)
+    }
+
+     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay.isKind(of: MKCircle.self){
+            let circleRenderer = MKCircleRenderer(overlay: overlay)
+            circleRenderer.fillColor = UIColor.blue.withAlphaComponent(0.2)
+            circleRenderer.strokeColor = UIColor.blue
+            circleRenderer.lineWidth = 1
+            return circleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
+}//end of randomLocation extension
 
 // must ask permission to use location-finder
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         checkLocationAuthorization()
     }
-}
+}//end of extension
 
 extension MapViewController {
+    
+     func locationServicesDisabledAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Check your location service settings and try again.", preferredStyle: .alert)
+
+         let dismissButton = UIAlertAction(title: "Okay", style: .default, handler: nil)
+
+         alert.addAction(dismissButton)
+        self.present(alert, animated: true)
+    }//end of locationServicesDisabledAlert func
+    
     func presentDeniedAlert() {
         let alert = UIAlertController(title: "Access Denied", message: "Check your permission settings and try again.", preferredStyle: .alert)
         
@@ -93,7 +124,7 @@ extension MapViewController {
         
         alert.addAction(dismissButton)
         self.present(alert, animated: true)
-    }
+    }//end of presentDenied Alert func
     
     func presentRestrictedAlert() {
         let alert = UIAlertController(title: "Access Restricted", message: "Permissions (i.e. Parental Controls) have been disabled for this user.", preferredStyle: .alert)
@@ -102,5 +133,5 @@ extension MapViewController {
         
         alert.addAction(dismissButton)
         self.present(alert, animated: true)
-    }
-}
+    }//end of presentRestrictedAlert func
+}//end of extension
