be9c9dc3811f376a9f7b212e6aea4c7aa50cd791
Theo Vora <theo.vora@gmail.com>
2020-05-14 11:57:52 -0600

    Move Chat view controllers to subfolder

---
+++ MVP/MVP/Controllers/View Controllers/Chat/ChatNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatNavigationController.swift
+//  MVP
+//
+//  Created by Theo Vora on 5/14/20.
+//  Copyright © 2020 coronacation. All rights reserved.
+//
+
+import UIKit
+
+class ChatNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
