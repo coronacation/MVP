8a2ce227de454979ca76cbe8d80e6c4f01002f5c
Theo Vora <theo.vora@gmail.com>
2020-07-06 21:17:51 -0600

    Add ChatListTableViewCell

---
+++ MVP/MVP/Controllers/View Controllers/Chat/ChatListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatListTableViewCell.swift
+//  MVP
+//
+//  Created by Theo Vora on 7/6/20.
+//  Copyright © 2020 coronacation. All rights reserved.
+//
+
+import UIKit
+
+class ChatListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
