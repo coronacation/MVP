3a59db516b295c79812d893968b007c4f8fef051
Theo Vora <theo.vora@gmail.com>
2020-07-09 15:50:11 -0600

    Replace Post Description TextView with UILabel in Home feed

--- MVP/MVP/Controllers/View Controllers/Feed/PostTableViewCell.swift
+++ MVP/MVP/Controllers/View Controllers/Feed/PostTableViewCell.swift
@@ -12,7 +12,7 @@ class PostTableViewCell: UITableViewCell {
     
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var descriptionTextView: TopAlignedLabel!
     @IBOutlet weak var datePostedLabel: UILabel!
     @IBOutlet weak var postUserFirstName: UILabel!
     
